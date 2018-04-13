464f0be4eaa53f56ec0686ec458e8a66a392d8ff
luciosblack <luciano.junior@live.com>
2018-04-13 16:13:16 -0300

    Inicio do Projeto.

---
+++ resources/layout/blank.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.material')
+
+@section('titulo')
+
+Informe o Título
+
+@endsection
+
+@section('content')
+
+
+<div class="row">
+
+</div> {{-- FIM ROW --}}
+
+@endsection
+
+@push('scripts')
+
+@endpush
