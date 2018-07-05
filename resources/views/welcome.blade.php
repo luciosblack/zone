d8712ca28b5fcdd6f0e1a8b9feea6251e0b637c5
luciosblack <luciano.junior@live.com>
2018-07-05 16:58:44 -0300

    Inclusão de KML
    Inclusão dos arquivos KML e do código para exibir as zonas no portal.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -122,7 +122,7 @@
 
     var myLatlng = new google.maps.LatLng(-22.782946,-43.431588);
         var mapOptions = {
-          zoom: 13,
+          zoom: 12,
           center: myLatlng,
           scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
           styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}]
@@ -136,7 +136,7 @@
 
         //camada de zoneamento
         var geoXmlAAC = new geoXML3.parser({map: map, zoom: false});
-            geoXmlAAC.parse('kml/AAC.kml');
+            geoXmlAAC.parse('kml/mesquita_zonas.kml');
   });
 </script>
 
