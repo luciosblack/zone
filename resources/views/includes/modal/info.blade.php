1a25e604b6b8d724ee609e14f90e19bb029ad4ca
luciosblack <luciano.junior@live.com>
2018-07-18 15:31:50 -0300

    Molda Info
    Estrutura da template modal info concluída.
    
    As informações principais serão exibidas em tabelas.
    
    As informações secundárias serão exibidas na futura template de relatório.

--- resources/views/includes/modal/info.blade.php
+++ resources/views/includes/modal/info.blade.php
@@ -1,12 +1,7 @@
 <div class="modal fade" id="modalInfo" tabindex="-1" role="dialog" aria-labelledby="modalInfoLabel">
 	<div class="modal-dialog modal-lg" role="document">
 	    <div class="modal-content">
-	      
-	      <div class="modal-header">
-	        <button class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-	        <h4 class="modal-title" id="modalInfoLabel">Certidão de Zoneamento</h4>
-	      </div>
-				
+
 				{{---------------------------- Modal Body --}}
 	      <div class="modal-body">
 	        <ul class="nav nav-pills nav-pills-primary" role="tablist">
@@ -15,51 +10,62 @@
 				        	Zoneamento
 				        </a>
 				    </li>
+				    <li class="nav-item">
+				        <a class="nav-link" data-toggle="tab" href="#link2" role="tablist" aria-expanded="true">
+				        	Daddos informados pelo técnico
+				        </a>
+				    </li>
 					</ul>
 					<div class="tab-content tab-space">
-				    
 				    <div class="tab-pane active" id="link1" aria-expanded="true">
-					    <div class="card">
-					    	<h4 class="card-header card-header-primary text-center">Identifação da consulta</h4>
-					    	<div class="card-body">
-						      <div class="form-group row">
-									 <label class="col-sm-2 col-form-label">Logradouro :</label>
-									 <label class="col-sm-10 col-form-label">Teste</label>
-									</div>
-									<div class="form-group row">
-									 <label class="col-sm-2 col-form-label">Bairro :</label>
-									 <label class="col-sm-10 col-form-label">Teste</label>
-									</div>
-								</div>
-							</div>
-
-							<div class="card">
-					    	<h4 class="card-header card-header-primary text-center">Zoneamento</h4>
-					    	<div class="card-body">
-					    		<h4 class="card-title text-center">A.O.P.1 - ÁREA DE OCUPAÇÃO PRIORITÁRIA 1</h4>
-						      <div class="form-group row">
-									 <label class="col-sm-10 col-form-label">R1 - Residencial Unifamiliar</label>
-									 <label class="col-sm-2 col-form-label pull-right">Tolerado</label>
-									</div>
-								</div>
-							</div>
-
-							<div class="card">
-					    	<h4 class="card-header card-header-primary text-center">Daddos informados pelo técnico</h4>
-					    	<div class="card-body">
-						      <div class="form-group row">
-									 <label class="col-sm-3 col-form-label pull-left">Testada Mínima</label>
-									 <label class="col-sm-9 col-form-label pull-right">10,00 metros</label>
-									</div>
-								</div>
-							</div>
+			    		<div class="table-responsive">
+				    		<table class="table table-hover table-sm">
+				    			<thead>
+				    				<tr>
+				    					<th>Zona</th>
+				    					<th class="text-right">A.O.P.1</th>
+				    					<th class>ÁREA DE OCUPAÇÃO PRIORITÁRIA 1</th>
+				    				</tr>
+				    			</thead>
+				    			<tbody>
+				    				<tr>
+				    					<td>R1</td>
+				    					<td>Residencial Unifamiliar</td>
+				    					<td class="text-right">Tolerado</td>
+				    				</tr>
+				    				<tr>
+				    					<td>R1</td>
+				    					<td>Residencial Unifamiliar</td>
+				    					<td class="text-right">Tolerado</td>
+				    				</tr>
+				    			</tbody>
+				    		</table>
+				    	</div>
+						</div>
+						<div class="tab-pane" id="link2" aria-expanded="true">
+							<div class="table-responsive">
+				    		<table class="table table-hover table-sm">
+				    			<tbody>
+				    				<tr>
+				    					<td></td>
+				    					<td>Testada Mínima</td>
+				    					<td>10,00 metros</td>
+				    				</tr>
+				    				<tr>
+				    					<td></td>
+				    					<td>Área Total Mínima do Lote</td>
+				    					<td>125,00 m² cada lote</td>
+				    				</tr>
+				    			</tbody>
+				    		</table>
+				    	</div>
 						</div>
 					</div>
 	      </div> {{-- Fim modal Body --}}
 	      
-	      <div class="modal-footer">
-	        <button class="btn btn-primary btn-link" data-dismiss="modal">Close</button>
-	        <a href="#" target="_blank" class="btn btn-primary btn-link pull-left">Imprimir</a>
+	      <div class="modal-footer d-flex">
+	        <button class="btn btn-primary btn-link" data-dismiss="modal">Fechar</button>
+	        <a href="#" target="_blank" class="btn btn-primary btn-link ml-auto">Imprimir</a>
 	      </div>
 
 	    </div> {{-- Fim modal-content --}}
