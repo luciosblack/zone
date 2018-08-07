ea007de22797a954241a8c4f076cdb6ef4a45fa2
luciosblack <luciano.junior@live.com>
2018-08-07 14:44:22 -0300

    Responsivo.
    Top bar configurada para responsividade em dispositivos móveis.

--- resources/views/includes/layouts/topbar.blade.php
+++ resources/views/includes/layouts/topbar.blade.php
@@ -1,28 +1,24 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-absolute fixed-top back-branco hide" id="top-bar" color-on-scroll="500">
-	<div class="container-fluid">
- 	<a href="http://www.mesquita.rj.gov.br/pmm/" target="_blank">
+
+	<a href="http://www.mesquita.rj.gov.br/pmm/" target="_blank" class="btn btn-link">
 		<img class="logo" src="{{ asset("img/Logotipo-Horizontal-Colorido-PMM.png") }}">
  	</a>
-
- 	<div class="col-sm-6">
-    <div class="navbar-form">
-      <div id="pac-card" class="input-group has-success">
-      	<button id="pac-button" type="submit" class="btn btn-success btn-round btn-link btn-just-icon">
-        	<i class="material-icons">search</i>
-          <div class="ripple-container"></div>
-        </button>
-        <input id="pac-input" type="text" class="form-control" placeholder="Clique aqui e digite o endereço">
-      </div>
+  
+  <div class="col-sm flex-item">
+    <div id="pac-card" class="input-group has-success">
+    	<button id="pac-button" type="submit" class="btn btn-success btn-round btn-link btn-just-icon">
+      	<i class="material-icons">search</i>
+        <div class="ripple-container"></div>
+      </button>
+      <input id="pac-input" type="text" class="form-control" placeholder="Clique aqui e digite o endereço">
     </div>
-    <ul class="navbar-nav">
-      <li class="nav-item"></li>
-    </ul>
-	</div>
+  </div>
 
-	
+  <a class="btn btn-link flex-item">
 		<img class="logo360" src="{{ asset("img/loading.gif") }}">
- 	
+	</a>
+
 </nav>
 <!-- End Navbar -->
 
